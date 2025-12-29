5d6e10d20597db63eefe1a5926d7d25656d1aa5c
Froglegs <41251184+MrFroglegs@users.noreply.github.com>
2025-12-28 20:09:05 -0800

    Ejected Shells pt.3
    -Added a new handgun caliber shell.

--- lua/weapons/arc9_cod2019_sm_mp5.lua
+++ lua/weapons/arc9_cod2019_sm_mp5.lua
@@ -272,11 +272,11 @@ SWEP.ProceduralViewQCA = 1
 SWEP.CamQCA = 4
 SWEP.CamQCA_Mult = 1
 
-SWEP.ShellModel = "models/weapons/cod2019/shared/shell_9mm_hr.mdl"
-SWEP.ShellCorrectAng = Angle(0, -90, 0)
-SWEP.ShellScale = 0.05
-SWEP.ShellPhysBox = Vector(0.5, 0.5, 2)
+SWEP.ShellModel = "models/weapons/cod2019/shared/fx_pistol_shell.mdl"
 SWEP.ShellSounds = ARC9.COD2019_9mm_Table
+SWEP.ShellCorrectAng = Angle(0, -90, 0)
+SWEP.ShellScale = 1
+SWEP.ShellPhysBox = Vector(0.5, 0.5, 2)
 
 SWEP.ShouldDropMag = false
 SWEP.ShouldDropMagEmpty = false
--- lua/weapons/arc9_cod2019_sm_striker45.lua
+++ lua/weapons/arc9_cod2019_sm_striker45.lua
@@ -274,10 +274,10 @@ SWEP.ProceduralViewQCA = 1
 SWEP.CamQCA = 4
 SWEP.CamQCA_Mult = 1
 
-SWEP.ShellModel = "models/weapons/cod2019/shared/shell_9mm_hr.mdl"
+SWEP.ShellModel = "models/weapons/cod2019/shared/fx_pistol_shell.mdl"
 SWEP.ShellSounds = ARC9.COD2019_9mm_Table
 SWEP.ShellCorrectAng = Angle(0, -90, 0)
-SWEP.ShellScale = 0.06
+SWEP.ShellScale = 1.2
 SWEP.ShellPhysBox = Vector(0.5, 0.5, 2)
 
 SWEP.ShouldDropMag = false
